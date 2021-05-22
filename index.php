d623e2e2fa661d383eae658d47adfa77f8f6b122
black92i <60386574+black92i@users.noreply.github.com>
2021-05-22 02:30:48 +0200

    z

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <html>
 <body>
-<iframe src="pdf/ct2021.pdf" style="width:100%; height:100%; frameBorder=0" ></iframe>
+<iframe src="pdf/ct2021.pdf" style="width:600px; height:500px;" frameborder="0" ></iframe>
 </body>
 </html>
