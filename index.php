d1b6322c9a068ec418576b0ca1f9a36fbe66dc4f
black92i <60386574+black92i@users.noreply.github.com>
2021-05-22 12:13:01 +0100

    Update index.php

--- index.php
+++ index.php
@@ -129,7 +129,7 @@
             </mask>
         </defs>
     </svg>
-    <div id="jssor_1" style="position:relative;margin:0 auto;bottom:0px;top:0px;left:0px;width:400px;height:500px;visibility:hidden;overflow-y: hidden;">
+    <div id="jssor_1" style="position:relative;margin:0 auto;bottom:0px;top:0px;left:0px;width:400px;height:812px;visibility:hidden;overflow-y: hidden;">
         <!-- Loading Screen -->
         <div data-u="loading" class="jssorl-009-spin" style="position:absolute;top:0px;left:0px;width:100%;height:100%;text-align:center;background-color:rgba(0,0,0,0.7);">
             <img style="margin-top:-19px;position:relative;top:50%;width:38px;height:38px;" src="img/spin.svg" />
