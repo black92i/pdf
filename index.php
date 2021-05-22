a67fbff5766caf2526a130c28a52425da8a3c838
black92i <60386574+black92i@users.noreply.github.com>
2021-05-22 02:33:33 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <html>
-<body>
-<iframe src="pdf/ct2021.pdf" style="width:600px; height:500px;" frameborder="0" ></iframe>
+<body style="margin: 0; padding: 0; border: 0;">
+<iframe src="pdf/ct2021.pdf" style="width:100%; height:100%;" frameborder="0" ></iframe>
 </body>
 </html>
